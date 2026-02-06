0337598e332fcb553be94b496279cce2e119d629
Thad Hughes <thad@imbue.com>
2026-02-05 16:33:21 -0800

    Update ASR_COMMIT_HASH for transcribe_cli entry point

--- electron/constants.ts
+++ electron/constants.ts
@@ -7,5 +7,5 @@ export const APP_VERSION = '1.3.7'
 
 // AI Transcription (uvx / transcribe)
 export const UV_VERSION = '0.7.12'
-export const ASR_COMMIT_HASH = 'ae7f3a66a43391a1973fc652c507e911bce12ec1'
+export const ASR_COMMIT_HASH = 'cc271397de931621a18e9b6a3b33094a79b392bb'
 export const ASR_GITHUB_REPO = 'git+https://github.com/thadd3us/caption_editor'
