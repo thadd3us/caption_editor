4a519b87bdd42493831f467b5041106cc866b34d
Thad Hughes <thad.hughes@gmail.com>
2026-02-15 10:03:39 -0800

    hash

--- electron/constants.ts
+++ electron/constants.ts
@@ -9,5 +9,5 @@ export const APP_VERSION = '1.3.18'
 export const UV_VERSION = '0.10.2'
 // NOTE: This hash must exist on GitHub because uvx fetches from ASR_GITHUB_REPO.
 // Keep this pinned to a pushed commit that includes the `transcribe` packaging config.
-export const ASR_COMMIT_HASH = '78eaf5545a5741fd1bf66a21e6a84e72ea7b4110'
+export const ASR_COMMIT_HASH = '63d63e1e57ea2f867caaa6b7b71ba71c9572bb1e'
 export const ASR_GITHUB_REPO = 'git+https://github.com/thadd3us/caption_editor'
